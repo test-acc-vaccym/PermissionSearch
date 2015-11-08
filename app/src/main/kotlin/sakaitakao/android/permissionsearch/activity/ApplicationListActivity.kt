8cc1f08fc163c4c209e80a00e470aba3fe450d89
Sakai Takao <s.takao.73@gmail.com>
2015-11-08 14:41:51 +0900

    Refactor.

--- app/src/main/kotlin/sakaitakao/android/permissionsearch/activity/ApplicationListActivity.kt
+++ app/src/main/kotlin/sakaitakao/android/permissionsearch/activity/ApplicationListActivity.kt
@@ -15,9 +15,9 @@ import sakaitakao.android.permissionsearch.R
 import sakaitakao.android.permissionsearch.adaptor.ApplicationListAdaptor
 import sakaitakao.android.permissionsearch.entity.ContextMenuItem
 import sakaitakao.android.permissionsearch.entity.PermissionInfoEx
+import sakaitakao.android.permissionsearch.util.ClosableLoan
 import sakaitakao.android.permissionsearch.util.PermissionInfoUtil
 import java.io.ByteArrayOutputStream
-import java.io.IOException
 import java.io.PrintWriter
 
 /**
@@ -27,13 +27,14 @@ import java.io.PrintWriter
  */
 class ApplicationListActivity : Activity() {
 
-    private val contextMenuItems = arrayOf(ContextMenuItem(CONTEXT_MENU_ITEM_ID_SHARE,
-            R.string.context_menu_item_share, android.R.drawable.ic_menu_share, OnMenuItemClickShareListener()))
+    private val contextMenuItems = arrayOf(
+            ContextMenuItem(
+                    CONTEXT_MENU_ITEM_ID_SHARE,
+                    R.string.context_menu_item_share,
+                    android.R.drawable.ic_menu_share,
+                    OnMenuItemClickShareListener())
+    )
 
-    /**
-     * アプリケーションリストのアダプタ実装
-     */
-    private var applicationListAdaptor: ApplicationListAdaptor? = null
 
     private var permissionInfoEx: PermissionInfoEx? = null
 
@@ -65,9 +66,7 @@ class ApplicationListActivity : Activity() {
 	 * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
 	 */
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        for (contextMenuItem in contextMenuItems) {
-            contextMenuItem.addToMenu(menu)
-        }
+        contextMenuItems.forEach { it.addToMenu(menu) }
         return super.onCreateOptionsMenu(menu)
     }
 
@@ -76,8 +75,9 @@ class ApplicationListActivity : Activity() {
     // -------------------------------------------------------------------------
     private val condition: PermissionInfoEx
         get() {
-            val intent = intent
-            val permissionInfoEx = intent.getParcelableExtra<PermissionInfoEx>(INTENTEXTRA_PERMISSION_APP_LIST) ?: throw RuntimeException("Please set extra data by Intent#putExtra($INTENTEXTRA_PERMISSION_APP_LIST, Parcelable)")
+            val int = intent
+            val permissionInfoEx = int.getParcelableExtra<PermissionInfoEx>(INTENTEXTRA_PERMISSION_APP_LIST) ?:
+                    throw RuntimeException("Please set extra data by Intent#putExtra($INTENTEXTRA_PERMISSION_APP_LIST, Parcelable)")
             return permissionInfoEx
         }
 
@@ -107,14 +107,16 @@ class ApplicationListActivity : Activity() {
 
     private fun showList(permissionInfoEx: PermissionInfoEx) {
         val appListView = findViewById(R.id.application_list_list) as ListView
-        applicationListAdaptor = ApplicationListAdaptor(this, R.layout.application_list_item, permissionInfoEx.applicationInfoList!!)
+        val applicationListAdaptor = ApplicationListAdaptor(this,
+                R.layout.application_list_item,
+                permissionInfoEx.applicationInfoList!!)
         appListView.adapter = applicationListAdaptor
     }
 
     private fun onOptionsItemShareSelected(): Boolean {
 
         // テキストを生成
-        val text = createPermissionInfoTextForSharing(packageManager, resources, permissionInfoEx!!)
+        val text = createTextForSharing(packageManager, resources, permissionInfoEx!!)
 
         // 共有の暗黙的インテントを発行
         val intent = Intent(android.content.Intent.ACTION_SEND)
@@ -139,48 +141,38 @@ class ApplicationListActivity : Activity() {
      * *
      * @return
      */
-    private fun createPermissionInfoTextForSharing(packageManager: PackageManager, resources: Resources, permissionInfoEx: PermissionInfoEx): String {
+    private fun createTextForSharing(packageManager: PackageManager, resources: Resources, permissionInfoEx: PermissionInfoEx): String {
 
-        var bos: ByteArrayOutputStream? = null
-        try {
-            bos = ByteArrayOutputStream()
-            val pw = PrintWriter(bos)
+        return ClosableLoan.using(ByteArrayOutputStream(), { bos ->
 
-            val permissionLabel = PermissionInfoUtil.getPermissionLabel(packageManager, resources, permissionInfoEx)
-            // タイトル
-            pw.println(resources.getString(R.string.application_list_share_title, permissionLabel))
-            pw.println()
+            ClosableLoan.using(PrintWriter(bos), { pw ->
 
-            // 概要
-            pw.println(resources.getString(R.string.application_list_share_permission_info, permissionLabel))
-            pw.println(PermissionInfoUtil.formatProtectionLevel(resources, permissionInfoEx))
-            pw.println(permissionInfoEx.permissionInfo?.loadDescription(packageManager))
-            pw.println()
+                val permissionLabel = PermissionInfoUtil.getPermissionLabel(packageManager, resources, permissionInfoEx)
+                // タイトル
+                pw.println(resources.getString(R.string.application_list_share_title, permissionLabel))
+                pw.println()
 
-            // applications
-            pw.println(resources.getString(R.string.application_list_share_application_list))
-            permissionInfoEx.applicationInfoList!!.forEach { applicationInfo ->
-                // App Name & link
-                pw.println(
-                        resources.getString(
-                                R.string.application_list_share_app_name_and_market_uri,
-                                applicationInfo.loadLabel(packageManager),
-                                applicationInfo.packageName)
-                )
-            }
+                // 概要
+                pw.println(resources.getString(R.string.application_list_share_permission_info, permissionLabel))
+                pw.println(PermissionInfoUtil.formatProtectionLevel(resources, permissionInfoEx))
+                pw.println(permissionInfoEx.permissionInfo?.loadDescription(packageManager))
+                pw.println()
 
-            pw.close()
-        } finally {
-            try {
-                bos?.close()
-            } catch (e: IOException) {
-                Log.e("", "IOException caught when closing ByteArrayOutputStream.", e)
-                throw RuntimeException(e)
-            }
+                // applications
+                pw.println(resources.getString(R.string.application_list_share_application_list))
+                permissionInfoEx.applicationInfoList!!.forEach {
+                    // App Name & link
+                    pw.println(resources.getString(
+                            R.string.application_list_share_app_name_and_market_uri,
+                            it.loadLabel(packageManager),
+                            it.packageName)
+                    )
+                }
+            })
 
-        }
-        Log.v("", bos?.toString())
-        return bos?.toString() ?: ""
+            Log.v("", bos.toString())
+            bos.toString()
+        }) as String
     }
 
     // -------------------------------------------------------------------------
---
+++ app/src/main/kotlin/sakaitakao/android/permissionsearch/util/ClosableLoan.kt
@@ -0,0 +1,24 @@
+package sakaitakao.android.permissionsearch.util
+
+import java.io.Closeable
+
+/**
+ * ローンパターンなのです
+ * Created by takao on 2015/11/08.
+ */
+object ClosableLoan {
+
+    /**
+     * ローンパターンなのです。
+     * @param s java.io.Closable を実装するオブジェクトなのです。
+     * @param f java.io.Closable を引数に取り何かを返す関数リテラルなのです。
+     * @return [f] が返した値なのです。
+     */
+    fun <A : Closeable> using(s: A, f: (A) -> Any): Any {
+        try {
+            return f(s)
+        } finally {
+            s.close()
+        }
+    }
+}
