228f39453d051a67a34333180ae9df109ef0fa1d
Sakai Takao <s.takao.73@gmail.com>
2015-11-21 14:59:11 +0900

    PreferenceFragment を使うよう変更。

--- app/src/main/kotlin/sakaitakao/android/permissionsearch/activity/MainPreferenceActivity.kt
+++ app/src/main/kotlin/sakaitakao/android/permissionsearch/activity/MainPreferenceActivity.kt
@@ -1,9 +1,10 @@
 package sakaitakao.android.permissionsearch.activity
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.preference.Preference
-import android.preference.PreferenceActivity
+import android.preference.PreferenceFragment
 import android.preference.PreferenceScreen
 import android.util.Log
 import sakaitakao.android.permissionsearch.R
@@ -13,39 +14,45 @@ import sakaitakao.android.permissionsearch.R
 
  * @author takao
  */
-class MainPreferenceActivity : PreferenceActivity() {
+class MainPreferenceActivity : Activity() {
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        addPreferencesFromResource(R.xml.preference)
+        fragmentManager.beginTransaction().replace(android.R.id.content, MainPreferenceFragment()).commit()
     }
 
-    /*
-	 * (非 Javadoc)
-	 * 
-	 * @seeandroid.preference.PreferenceActivity#onPreferenceTreeClick(android.
-	 * preference.PreferenceScreen, android.preference.Preference)
-	 */
-    override fun onPreferenceTreeClick(preferenceScreen: PreferenceScreen, preference: Preference): Boolean {
 
-        Log.v("onPreferenceTreeClick", "preference.key = " + preference.key)
+    /**
+     * 設定画面のフラグメント
+     */
+    class MainPreferenceFragment : PreferenceFragment() {
 
-        when (preference.key) {
-            SUB_PREFERENCE_SAVED_SEARCH_COND_KEY ->
-                startActivity(Intent(this, SavedAdvancedSearchConditionPreferenceActivity::class.java))
-
-            SUB_PREFERENCE_ABOUT_KEY ->
-                startActivity(Intent(this, AboutActivity::class.java))
-
-            else ->
-                Log.e("onPreferenceTreeClick", "Unknown Preference key ${preference.key}")
+        public override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            addPreferencesFromResource(R.xml.preference)
         }
-        return super.onPreferenceTreeClick(preferenceScreen, preference)
-    }
 
-    companion object {
+        override fun onPreferenceTreeClick(preferenceScreen: PreferenceScreen, preference: Preference): Boolean {
 
-        private val SUB_PREFERENCE_SAVED_SEARCH_COND_KEY = "saved_advanced_search_condition"
-        private val SUB_PREFERENCE_ABOUT_KEY = "about"
+            Log.v("onPreferenceTreeClick", "preference.key = " + preference.key)
+
+            when (preference.key) {
+                SUB_PREFERENCE_SAVED_SEARCH_COND_KEY ->
+                    startActivity(Intent(activity, SavedAdvancedSearchConditionPreferenceActivity::class.java))
+
+                SUB_PREFERENCE_ABOUT_KEY ->
+                    startActivity(Intent(activity, AboutActivity::class.java))
+
+                else ->
+                    Log.e("onPreferenceTreeClick", "Unknown Preference key ${preference.key}")
+            }
+            return super.onPreferenceTreeClick(preferenceScreen, preference)
+        }
+
+        companion object {
+
+            private val SUB_PREFERENCE_SAVED_SEARCH_COND_KEY = "saved_advanced_search_condition"
+            private val SUB_PREFERENCE_ABOUT_KEY = "about"
+        }
     }
 }
